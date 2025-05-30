0e506b799e78b42f24d8ef12f7ab5fa3f34d3280
winston-wen <winston-wen@hotmail.com>
2025-05-30 02:23:28 +0000

    拆分server和serverimpl, 添加grpc-netty依赖从而能正常启动

--- src/main/java/com/cregis/svarog/App.java
+++
@@ -1,8 +0,0 @@
-package com.cregis.svarog;
-import com.cregis.svarog.pb.*;
-
-public class App {
-    public static void main(String[] args) {
-        
-    }
-}
--- src/main/java/com/cregis/svarog/MpcSessionManagerServer.java
+++ src/main/java/com/cregis/svarog/MpcSessionManagerServer.java
@@ -2,25 +2,13 @@ package com.cregis.svarog;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
-import com.cregis.svarog.pb.MpcSessionManagerGrpc.MpcSessionManagerImplBase;
-import com.cregis.svarog.pb.Svarog.EchoMessage;
-import com.cregis.svarog.pb.Svarog.SessionConfig;
-import com.cregis.svarog.pb.Svarog.SessionId;
-import com.cregis.svarog.pb.Svarog.VecMessage;
-import com.cregis.svarog.pb.Svarog.Void;
+import java.util.logging.Logger;
 
 import io.grpc.Grpc;
 import io.grpc.InsecureServerCredentials;
 import io.grpc.Server;
-import io.grpc.stub.StreamObserver;
 
-// 在vscode里如何生成java的方法存根?
-// (1') 单击并悬浮在类名上.
-// (2') 按键 "Ctrl+.", 这会呼出一个菜单.
-// (3') 单击菜单中的 "Override/implement Methods...",
-//  这会弹出一个悬浮窗口供我选择函数名.
-// (4') 选择要实现的方法, 确认.
+
 public class MpcSessionManagerServer {
     public static void main(String[] args) throws Exception {
         var server = new MpcSessionManagerServer(3000);
@@ -29,17 +17,22 @@ public class MpcSessionManagerServer {
     }
 
     private final Server server_object;
+    private final int port;
+    private static final Logger logger = Logger.getLogger(
+            MpcSessionManagerServer.class.getName());
 
     public MpcSessionManagerServer(int port) {
+        this.port = port;
         var server_builder = Grpc.newServerBuilderForPort(
                 port,
                 InsecureServerCredentials.create());
-        var server_impl = new MpcSessionManagerService();
-        this.server_object = server_builder.addService(server_impl).build();
+        var impl = new MpcSessionManagerServerImpl();
+        this.server_object = server_builder.addService(impl).build();
     }
 
     public void start() throws IOException {
         server_object.start();
+        logger.info("SvarogManager started listening on " + this.port);
         var th = new Thread() {
             @Override
             public void run() {
@@ -73,42 +66,6 @@ public class MpcSessionManagerServer {
     private void blockUntilShutdown() throws InterruptedException {
         if (server_object != null) {
             server_object.awaitTermination();
-        }
-    }
-
-    private static class MpcSessionManagerService extends MpcSessionManagerImplBase {
-        @Override
-        public void getSessionConfig(SessionId req, StreamObserver<SessionConfig> resp_ob) {
-            // TODO Auto-generated method stub
-            SessionConfig resp = SessionConfig.newBuilder()
-                    .setSessionId("114514")
-                    .build();
-            resp_ob.onNext(resp);
-            resp_ob.onCompleted();
-        }
-
-        @Override
-        public void inbox(VecMessage req, StreamObserver<Void> resp_ob) {
-            // TODO Auto-generated method stub
-            super.inbox(req, resp_ob);
-        }
-
-        @Override
-        public void newSession(SessionConfig req, StreamObserver<SessionId> resp_ob) {
-            // TODO Auto-generated method stub
-            super.newSession(req, resp_ob);
-        }
-
-        @Override
-        public void outbox(VecMessage req, StreamObserver<VecMessage> resp_ob) {
-            // TODO Auto-generated method stub
-            super.outbox(req, resp_ob);
-        }
-
-        @Override
-        public void ping(Void req, StreamObserver<EchoMessage> resp_ob) {
-            // TODO Auto-generated method stub
-            super.ping(req, resp_ob);
         }
     }
 }
